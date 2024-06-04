94ef4063c9b748774908213115c7cb86ea30714c
Cameron Abo <cameroonie99@gmail.com>
2024-06-03 17:52:08 -0700

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -129,7 +129,7 @@ int main(int argc, char **argv) {
        else if (split_paths[1] == "echo") {
            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(split_paths[2].length()) + "\r\n\r\n" + split_paths[2];
        }
-       else if (get_user_agent(headers, user_agent) {
+       else if (get_user_agent(headers, user_agent)) {
            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(user_agent.length()) + "\r\n\r\n" + user_agent;
        }
        else {
