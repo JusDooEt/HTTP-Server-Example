5c319a7dd5034248850ab947d18321b6c676ac89
Cameron Abo <cameroonie99@gmail.com>
2024-06-03 18:16:57 -0700

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,9 @@
+#include <cstddef>
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <cstdlib>
+#include <map>
 #include <string>
 #include <cstring>
 #include <unistd.h>
@@ -7,134 +11,182 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <netdb.h>
-#include <vector>
-#include <sstream>
-
-std::vector<std::string> split_message(const std::string& message, const std::string& delim) {
-    std::vector<std::string> toks;
-    std::stringstream ss = std::stringstream{ message };
+#include <sys/epoll.h>
+#define MAX_CONNECTIONS 10
+#define BUFFER_SIZE 1024
+struct HTTPResponse {
+    std::string status;
+    std::string content_type;
+    std::map<std::string, std::string> headers;
+    std::string body;
+    std::string to_string() {
+        std::string response;
+        response += status + "\r\n";
+        response += "Content-Type: " + content_type + "\r\n";
+        for (const auto& header : headers) {
+            response += header.first + ": " + header.second + "\r\n";
+        }
+        response += "\r\n";
+        response += body;
+        return response;
+    }
+};
+struct HTTPRequest {
+    std::string method;
+    std::string path;
+    std::string version;
+    std::map<std::string, std::string> headers;
+    std::string body;
+};
+HTTPRequest parse_request(std::string request) {
+    HTTPRequest req;
+    std::stringstream ss(request);
     std::string line;
-    while (getline(ss, line, *delim.begin())) {
-        toks.push_back(line);
-        ss.ignore(delim.length() - 1);
+    std::getline(ss, line);
+    std::istringstream line_ss(line);
+    line_ss >> req.method >> req.path >> req.version;
+    while (std::getline(ss, line) && !line.empty()) {
+        size_t pos = line.find(":");
+        if (pos != std::string::npos) {
+            std::string header_name = line.substr(0, pos);
+            std::string header_value = line.substr(pos + 2);
+            header_value.erase(header_value.end() - 1);
+            req.headers[header_name] = header_value;
+        }
     }
-    return toks;
-}
-
-std::string get_path(std::string request) {
-    std::vector<std::string> toks = split_message(request, "\r\n");
-    std::vector<std::string> path_toks = split_message(toks[0], " ");
-    return path_toks[1];
-}
-
-std::vector<std::string> get_headers(const std::string& request) {
-    std::vector<std::string> headers;
-    std::vector<std::string> toks = split_message(request, "\r\n");
-    std::cout << "\nHeaders:\n";
-    for (int i = 1; *toks[i].begin() != 'A'; i++)
-    {
-        headers.push_back(toks[i]);
-        std::cout << headers.back() << std::endl;
+    std::stringstream body_ss;
+    std::getline(ss, line);
+    body_ss << line;
+    while (std::getline(ss, line)) {
+        body_ss << line << "\n";
+        body_ss << "\n" << line;
     }
-    return headers;
+    req.body = body_ss.str();
+    return req;
 }
-
-bool get_user_agent(std::vector<std::string>& headers, std::string& user_agent) {
-    std::string key = "User-Agent: ";
-    std::cout << "Headers Size = " << headers.size() << std::endl;
-    return false;
+void write_response(int client_fd, HTTPResponse response) {
+    std::string response_str = response.to_string();
+    ssize_t bytes_written = write(client_fd, response_str.c_str(), response_str.size());
+    if (bytes_written < 0) {
+        std::cerr << "Failed to write response to client\n";
+        close(client_fd);
+    }
 }
-
-int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  
-
-   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-   if (server_fd < 0) {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
-   }
-  
-   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-   // ensures that we don't run into 'Address already in use' errors
-   int reuse = 1;
-   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-     std::cerr << "setsockopt failed\n";
-     return 1;
-   }
-  
-   struct sockaddr_in server_addr;
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_addr.s_addr = INADDR_ANY;
-   server_addr.sin_port = htons(4221);
-  
-   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-     std::cerr << "Failed to bind to port 4221\n";
-     return 1;
-   }
-  
-   int connection_backlog = 5;
-   if (listen(server_fd, connection_backlog) != 0) {
-     std::cerr << "listen failed\n";
-     return 1;
-   }
-  
-   struct sockaddr_in client_addr;
-   int client_addr_len = sizeof(client_addr);
-  
-   std::cout << "Waiting for a client to connect...\n";
-  
-   int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_len);
-   if (client_fd < 0)
-   {
-       std::cerr << "error handling client connection\n";
-       close(server_fd);
-       return 1;
-   }
-   //std::string message = "HTTP/1.1 200 OK\r\n\r\n";
-   //send(client, message.c_str(), message.length(), 0);
-   std::cout << "Client connected\n";
-  
-   char buffer[1024];
-   int ret = read(client_fd, buffer, sizeof(buffer));
-   if (ret < 0) {
-       std::cerr << "Error in reading from client socket" << std::endl;
-   }
-   else if (ret == 0) {
-       std::cout << "No bytes read" << std::endl;
-   }
-   else {
-       std::string request(buffer);
-       std::cout << "Request: " << request << std::endl;
-       std::string path = get_path(request);
-       std::vector<std::string> split_paths = split_message(path, "/");
-       std::string user_agent;
-       // Creating a vector of headers and their data
-       std::vector<std::string> headers = get_headers(request);
-       //bool test = get_user_agent(headers, user_agent);
-       //std::cout << "test = " << std::to_string(test) << std::endl;
-
-       std::string response;
-       if (path == "/") {
-           response = "HTTP/1.1 200 OK\r\n\r\n";
-       }
-       else if (split_paths[1] == "echo") {
-           response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(split_paths[2].length()) + "\r\n\r\n" + split_paths[2];
-       }
-       /*else if (get_user_agent(headers, user_agent)) {
-           response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(user_agent.length()) + "\r\n\r\n" + user_agent;
-       }*/
-       else {
-           response = "HTTP/1.1 404 Not Found\r\n\r\n";
-       }
-
-       std::cout << "Response: " << response << std::endl;
-       send(client_fd, response.c_str(), response.length());
-   }
-
-   close(server_fd);
-   return 0;
+void handle_client(int client_fd, int epoll_fd) {
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer));
+    if (bytes_read <= 0) {
+        std::cerr << "Failed to read from client\n";
+        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
+        close(client_fd);
+        return;
+    }
+    std::string message(buffer, bytes_read);
+    std::cout << "Received message: " << message << std::endl;
+    HTTPRequest request = parse_request(message);
+    if (request.method == "GET") {
+        if (request.path == "/") {
+            HTTPResponse response = { "HTTP/1.1 200 OK", "text/plain", {}, "Hello, World!" };
+            write_response(client_fd, response);
+        }
+        else if (request.path == "/user-agent") {
+            std::string body = request.headers["User-Agent"];
+            HTTPResponse response = { "HTTP/1.1 200 OK", "text/plain", { {"Content-Length", std::to_string(body.length())} }, body };
+            write_response(client_fd, response);
+        }
+        else if (request.path.substr(0, 6) == "/echo/") {
+            std::string subStr = request.path.substr(6);
+            HTTPResponse response = { "HTTP/1.1 200 OK", "text/plain", { {"Content-Length", std::to_string(subStr.length())} }, subStr };
+            write_response(client_fd, response);
+        }
+        else if (request.path.substr(0, 7) == "/files/") {
+            std::string directory = ".";
+            std::string filename = request.path.substr(7);
+            std::ifstream file(directory + "/" + filename);
+            if (!file) {
+                HTTPResponse response = { "HTTP/1.1 404 Not Found", "text/plain", {}, "Not Found" };
+                write_response(client_fd, response);
+                return;
+            }
+            std::stringstream body;
+            body << file.rdbuf();
+            HTTPResponse response = { "HTTP/1.1 200 OK", "application/octet-stream", { {"Content-Length", std::to_string(body.str().length())} }, body.str() };
+            write_response(client_fd, response);
+        }
+        else {
+            HTTPResponse response = { "HTTP/1.1 404 Not Found", "text/plain", {}, "Not Found" };
+            write_response(client_fd, response);
+        }
+    }
+    else {
+        HTTPResponse response = { "HTTP/1.1 405 Method Not Allowed", "text/plain", {}, "Method Not Allowed" };
+        write_response(client_fd, response);
+    }
+}
+int main(int argc, char** argv) {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "Failed to create server socket\n";
+        return 1;
+    }
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+        std::cerr << "setsockopt failed\n";
+        return 1;
+    }
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(4221);
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
+        std::cerr << "Failed to bind to port 4221\n";
+        return 1;
+    }
+    int connection_backlog = 10;
+    if (listen(server_fd, connection_backlog) != 0) {
+        std::cerr << "listen failed\n";
+        return 1;
+    }
+    // Create an epoll instance
+    int epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1) {
+        std::cerr << "Failed to create epoll instance\n";
+        return 1;
+    }
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = server_fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+        std::cerr << "Failed to add server socket to epoll instance\n";
+        return 1;
+    }
+    struct epoll_event events[MAX_CONNECTIONS];
+    while (true) {
+        int num_fds = epoll_wait(epoll_fd, events, MAX_CONNECTIONS, -1);
+        for (int i = 0; i < num_fds; i++) {
+            if (events[i].data.fd == server_fd) {
+                struct sockaddr_in client_addr;
+                socklen_t client_addr_len = sizeof(client_addr);
+                // Accept the connection
+                int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+                if (client_fd == -1) {
+                    std::cerr << "Failed to accept connection\n";
+                    continue;
+                }
+                std::cout << "Client connected\n";
+                event.events = EPOLLIN;
+                event.data.fd = client_fd;
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event) == -1) {
+                    std::cerr << "Failed to add client socket to epoll instance\n";
+                    close(client_fd);
+                    continue;
+                }
+            }
+            else {
+                handle_client(events[i].data.fd, epoll_fd);
+            }
+        }
+    }
+    close(server_fd);
+    return 0;
 }
